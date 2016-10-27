9aeed2584548ed6808855d0b52dce93b1775ec95
Tobias <tobias_maier99@gmx.de>
2016-10-27 18:42:07 +0200

    added increase/decrease and some tests

--- showview.py
+++ showview.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 """
 showview.py
 
@@ -19,6 +20,7 @@ class ShowView():
     def __init__(self, sourcefile=SOURCE):
         """ reads the xml file """
 
+        self.sourcefile = sourcefile
         self.tree = ET.parse(sourcefile)
         self.root = self.tree.getroot()
 
@@ -39,32 +41,106 @@ class ShowView():
                 show = _show
                 break
         else:
-            raise(Exception("No show found for name '" + name + "'"))
+            raise KeyError("No show found for name '{}'".format(name))
 
         return {"name": show.find('Name').text,
                 "season": int(show.find('Season').text),
                 "episode": int(show.find('Episode').text)}
+
+    def set_show(self,
+                 show=None,
+                 name=None,
+                 season=None,
+                 episode=None,
+                 new_name=None):
+        """ update a show with new values """
+
+        if not (show or name):
+            raise LookupError("Name or show must be set")
+
+        if not show:
+            show = dict()
+
+        if name:
+            show['name'] = name
+        if season:
+            show['season'] = season
+        if episode:
+            show['episode'] = episode
+        if new_name:
+            show['new_name'] = new_name
+
+        for _show in self.root.findall('Show'):
+            if _show.find('Name').text == show['name']:
+                if 'new_name' in show:
+                    _show.find('Name').text = str(show['new_name'])
+                if 'season' in show:
+                    _show.find('Season').text = str(show['season'])
+                if 'episode' in show:
+                    _show.find('Episode').text = str(show['episode'])
+
+    def write_shows(self):
+        """ write the xml back to the file """
+
+        self.tree.write(self.sourcefile)
 
 
 def main():
     """ this function gets called from the commandline """
 
     ap = argparse.ArgumentParser()
-    ap.add_argument('--showfile', nargs='?', help='the xmlfile with the shows')
-    ap.add_argument('show', nargs='?', help='select a single show')
+    ap.add_argument('-ie', '--incepisode',
+                    action='store_true',
+                    help='increase the episode by one')
+    ap.add_argument('-is', '--incseason',
+                    action='store_true',
+                    help='increase the season by one')
+    ap.add_argument('-de', '--decepisode',
+                    action='store_true',
+                    help='increase the episode by one')
+    ap.add_argument('-ds', '--decseason',
+                    action='store_true',
+                    help='increase the season by one')
+    ap.add_argument('--showfile',
+                    nargs='?',
+                    default=SOURCE,
+                    help='the xmlfile with the shows')
+    ap.add_argument('show',
+                    nargs='?',
+                    help='select a single show')
     args = ap.parse_args()
 
-    if args.show:
-        show = ShowView(args.showfile).get_show(args.show)
-        print("{:40} {:2} - {:2}".format(show["name"],
-                                         show["season"],
-                                         show["episode"]))
+    showview = ShowView(args.showfile)
 
-    else:
-        for show in ShowView(args.showfile).get_shows():
+    try:
+        if args.show:
+            show = showview.get_show(args.show)
+            if args.incepisode:
+                show['episode'] += 1
+                showview.set_show(show)
+                showview.write_shows()
+            if args.incseason:
+                show['season'] += 1
+                showview.set_show(show)
+                showview.write_shows()
+            if args.decepisode:
+                show['episode'] -= 1
+                showview.set_show(show)
+                showview.write_shows()
+            if args.decseason:
+                show['season'] -= 1
+                showview.set_show(show)
+                showview.write_shows()
             print("{:40} {:2} - {:2}".format(show["name"],
                                              show["season"],
                                              show["episode"]))
+        else:
+            for show in showview.get_shows():
+                print("{:40} {:2} - {:2}".format(show["name"],
+                                                 show["season"],
+                                                 show["episode"]))
+    except KeyError as e:
+        print(e)
 
 
 if __name__ == '__main__':
--- test_showview.py
+++ test_showview.py
@@ -2,7 +2,6 @@
 tests for showview
 """
 
-import builtins
 import sys
 from unittest import TestCase
 from unittest.mock import patch, call
@@ -13,6 +12,7 @@ TESTXML = './test.xml'
 
 
 def side_effect(arg):
+    """ helper for mock """
     return arg
 
 
@@ -34,6 +34,12 @@ class TestSingleShow(TestCase):
         self.assertEqual(self.showview.get_show('test2'),
                          {"name": "test2", "season": 10, "episode": 10})
 
+    def test_missing_show(self):
+        """ try to get a show that doesn't exist """
+
+        with self.assertRaises(KeyError):
+            self.showview.get_show('NotExisting')
+
 
 class TestAllShows(TestCase):
     """ tests the basic behaviors """
@@ -49,6 +55,43 @@ class TestAllShows(TestCase):
                           {"name": "test2", "season": 10, "episode": 10}])
 
 
+class TestChangeShow(TestCase):
+    """ test the basic behavior to change a show and write to the file """
+
+    def setUp(self):
+        self.showview = ShowView(TESTXML)
+
+    def test_change_episode(self):
+        """ get one show and change the episode """
+
+        self.showchange = self.showview.get_show('test1')
+        self.showchange['episode'] = 2
+        self.showview.set_show(self.showchange)
+        self.assertEqual(self.showview.get_show('test1'),
+                         {"name": "test1", "season": 1, "episode": 2})
+
+    def test_change_season(self):
+        """ get one show and change the season """
+
+        self.showchange = self.showview.get_show('test1')
+        self.showchange['season'] = 5
+        self.showview.set_show(self.showchange)
+        self.assertEqual(self.showview.get_show('test1'),
+                         {"name": "test1", "season": 5, "episode": 1})
+
+    def test_change_name(self):
+        """ get one show and change the name """
+
+        self.showchange = self.showview.get_show('test1')
+        self.showchange['new_name'] = 'test_new'
+        self.showview.set_show(self.showchange)
+        with self.assertRaises(KeyError):
+            self.showview.get_show('test1')
+        self.showafter = self.showview.get_show('test_new')
+        self.assertEqual(self.showview.get_show('test_new'),
+                         {"name": "test_new", "season": 1, "episode": 1})
+
+
 class TestMain(TestCase):
     """ test the main function print statements """
 
@@ -56,9 +99,9 @@ class TestMain(TestCase):
         self.showview = ShowView(TESTXML)
         sys.argv = ['showview.py', '--showfile', TESTXML]
 
-
     @patch('builtins.print')
     def test_main_without_show(self, mock_print):
+        """ should return every show from the xml file """
         main()
         expected = [call('test1                                     1 -  1'),
                     call('test2                                    10 - 10')]
@@ -67,6 +110,18 @@ class TestMain(TestCase):
 
     @patch('builtins.print', side_effect=side_effect)
     def test_main_with_show(self, mock_print):
+        """ should return one show from the xml file """
         sys.argv.append('test1')
         main()
-        mock_print.assert_called_with('test1                                     1 -  1')
+        mock_print.assert_called_with(
+            'test1                                     1 -  1')
+
+    # @patch('builtins.print', side_effect=side_effect)
+    # def test_main_with_missing_show(self, mock_print):
+        # """ should return an error string about the missing show """
+        # main()
+        # sys.argv.append('test_missing')
+        # main()
+
+        # mock_print.assert_called_with(
+            # KeyError("No show found for name 'test_missing'"))
